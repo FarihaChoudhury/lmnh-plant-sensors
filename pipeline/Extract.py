0e67339bc7231ef3aeb5a5d1cd6b02795ca40927
Keogh Jokhan <trainee.keogh.jokhan@sigmalabs.co.uk>
2024-11-25 21:55:18 +0000

    Consistent formatting

--- pipeline/Extract.py
+++ pipeline/Extract.py
@@ -4,23 +4,20 @@ import os
 import csv
 import requests
 
-def extract_botanist_information(botanist_info: dict):
+def extract_botanist_information(botanist_info: dict) -> dict:
     """Extracts the botanist information from the API JSON"""
-    Botanist = {"name" : "", "email" : "", "phone" : ""}
-    for key, value in botanist_info.items():
-        if key in Botanist:
-            Botanist[key] = value
-    return Botanist
+    return {"name" : botanist_info.get('name'),
+        "email" : botanist_info.get('email'),
+        "phone" : botanist_info.get('phone')}
 
-def extract_location_information(location_info: list):
+def extract_location_information(location_info: list) -> dict:
     """Extracts the location information from the API JSON"""
     return {"longitude" : location_info[0], 
                 "latitude" : location_info[1], 
                 "closest_town" : location_info[2], 
-                "ISO_code" : location_info[3]
-                }
+                "ISO_code" : location_info[3]}
 
-def extract_plant_information(plant_info: dict):
+def extract_plant_information(plant_info: dict) -> dict:
     """Extracts the plant information from the API JSON"""
     scientific_name = plant_info.get('scientific_name')
     images = plant_info.get('images')
@@ -39,14 +36,14 @@ def extract_plant_information(plant_info: dict):
              "scientific_name" : scientific_name, 
              "original_url" : images}
 
-def extract_metric_information(metric_info: dict):
+def extract_metric_information(metric_info: dict) -> dict:
     """Extracts the plant metric information from the API JSON"""
     return {"temperature" : metric_info['temperature'],
             "soil_moisture" : metric_info['soil_moisture'],
             "recording_taken" : metric_info['recording_taken'],
             "last_watered": metric_info['last_watered']}
 
-def fetch_and_collect_data():
+def fetch_and_collect_data() -> None:
     """Fetches data from the API and collects it into a list of dictionaries"""
     collected_data = []
     for number in range(50):
@@ -76,13 +73,12 @@ def fetch_and_collect_data():
     return collected_data
 
 
-def write_to_csv(data: list[dict], csv_file: str):
+def write_to_csv(data: list[dict], csv_file: str) -> None:
     """Writes the collected data to the CSV file"""
     with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.DictWriter(file, fieldnames=data[0].keys()) # gets column headings from first csv row
-        writer.writeheader() # writes them
+        writer.writeheader() 
         writer.writerows(data)
-
 
 if __name__ == "__main__":
     csv_file = "Api_information.csv"
