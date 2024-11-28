2a22b51550561abdaaed977daa0540e1bfeddd6d
SurinaCS <surinasanthokhy@yahoo.co.uk>
2024-11-28 15:48:16 +0000

    3 tests that actually pass

--- dashboard/test_queries.py
+++ dashboard/test_queries.py
@@ -15,8 +15,18 @@ class TestingDBQueries:
     def mock_cursor(self):
         """Mock cursor to avoid real world db connections."""
         return Mock()
-
-    @patch('load.connect')
+    @patch.dict(
+        environ,
+        {
+            "DB_HOST": "mock_value",
+            "DB_NAME": "mock_value",
+            "DB_USER": "mock_value",
+            "DB_PASSWORD": "mock_value",
+            "DB_PORT": "mock_value",
+            "SERVER": "mock_value"
+        },
+    )
+    @patch('db_queries.connect')
     def test_get_connection_success(self, mock_connect):
         """Test successful database connection."""
 
@@ -45,7 +55,7 @@ class TestingDBQueries:
             "SERVER": "mock_value"
         },
     )
-    @patch('load.connect')
+    @patch('db_queries.connect')
     def test_get_connection_missing_env_var(self, mock_connect):
         """Test name key error when connecting to the database."""
         with pytest.raises(KeyError) as error:
@@ -64,7 +74,7 @@ class TestingDBQueries:
             "DB_PORT": "mock_value",
         },
     )
-    @patch('load.connect')
+    @patch('db_queries.connect')
     def test_get_connection_operational_error(self, mock_connect):
         """Test OperationalError when database connection fails."""
         mock_connect.side_effect = exceptions.OperationalError(
@@ -82,6 +92,8 @@ class TestingDBQueries:
             as_dict=True
         )
         assert str(error.value) == "Operational error occurred"
+
+        
 
 # tests to write
 # test valid connection
