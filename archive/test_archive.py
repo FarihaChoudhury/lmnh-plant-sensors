949a87f0e226b4658b89cca32c929e3388073480
Fariha <trainee.fariha.choudhury@sigmalabs.co.uk>
2024-11-28 12:55:30 +0000

    aded test file

---
+++ archive/test_archive.py
@@ -0,0 +1,6 @@
+from archive import (get_connection, get_all_plant_ids,
+                     get_plants_data, upload_plant_metric_data, calculate_archive_metrics, get_latest_recording, clear_plant_metrics)
+
+
+class TestArchive():
+    ...
